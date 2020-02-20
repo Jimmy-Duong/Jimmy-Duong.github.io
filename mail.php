e64a13cfc3540e839618c5690262b904123ef050
Jimmy Duong <jimmyff12@gmail.com>
2020-02-20 16:21:39 -0500

    Update Thank you Message and link to EmailForm.html

--- mail.php
+++ mail.php
@@ -1,4 +1,5 @@
-<?php $name = $_POST['name'];
+<?php 
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
@@ -6,5 +7,5 @@ $recipient = "emailaddress@here.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='EmailForm.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
